357f0ce7b564eb73a4274ed9cdf709976e94231e
Andri011202 <103478515+Andri011202@users.noreply.github.com>
2022-10-04 18:38:12 +0700

    Create PRAK105_2110817110008_AndriRahmadani.java

---
+++ PRAK105_2110817110008_AndriRahmadani.java
@@ -0,0 +1,31 @@
+package praktikum.pertama;
+
+import java.util.Scanner;
+
+public class PRAK105_2110817110008_AndriRahmadani {
+    public static void main(String[] args) {
+        //Konstanta PHI
+        final float PHI = 3.14f;
+
+        /* Variable:
+        r untuk jari-jari
+        t untuk tinggi dan volume
+         */
+        float r, t, volume;
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("=====INPUT=====");
+        System.out.print("Masukan jari-jari \t : ");
+        r = keyboard.nextFloat();
+
+        System.out.print("Masukan tinggi \t\t : ");
+        t = keyboard.nextFloat();
+
+        //Rumus volume tabung
+        volume = PHI * r * r * t;
+
+        //Output
+        System.out.println("=====OUTPUT=====");
+        System.out.printf("Volume tabung dengan jari-jari %.1f cm dan tinggi %.1f cm adalah %.3f m3",r,t,volume);
+    }
+}
