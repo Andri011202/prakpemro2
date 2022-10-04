420989f476141a0613d149c12d1805966ac2341c
Andri011202 <103478515+Andri011202@users.noreply.github.com>
2022-10-04 18:35:43 +0700

    Create PRAK103_2110817110008_AndriRahmadani.java

---
+++ PRAK103_2110817110008_AndriRahmadani.java
@@ -0,0 +1,22 @@
+package praktikum.pertama;
+
+import java.util.Scanner;
+
+public class PRAK103_2110817110008_AndriRahmadani {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner (System.in);
+        int input = keyboard.nextInt();
+        int i = 1;
+        do {
+            //Pengondisian apabila deret habis dibagi 7
+            if (input % 7 == 0) {
+                //Akan mengabaikan dan kembali ke atas
+                continue;
+            }
+            if (i == 5) System.out.print(input);
+            else System.out.print(input + ", ");
+            i++;
+            input--;
+        } while (i<=5);
+    }
+}
